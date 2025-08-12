1a704f7a6aa1427f080283426aef1428c14b7415
Flávio Gomes <flavio.santos.gomes@escola.pr.gov.br>
2025-08-12 10:39:56 -0300

    Adição das Consultas feitas nas tabelas

---
+++ Consultas Relevantes.sql
@@ -0,0 +1,12 @@
+select * from pagamentos;
+select valor from pagamentos where valor > 5.50;
+select * from pagamentos WHERE valor BETWEEN 5.80 AND 10.80;
+
+select * from clientes;
+SELECT * from clientes where nome like 'F%' order by nome;
+select * from clientes where id >2;
+
+select * from filmes;
+select * from filmes where Classificação >14;
+select Título, Gênero from filmes;
+
